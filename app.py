ab7ed2d3bb8f5f07aa3da4c3c68e1bd1a293b330
JenPingLee <99060653+JenPingLee@users.noreply.github.com>
2022-10-11 23:14:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from importlib.resources import open_binary, read_binary
+# from importlib.resources import open_binary, read_binary
 import streamlit as st
 import pandas as pd 
 import numpy as np
